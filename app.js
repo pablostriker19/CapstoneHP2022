b159d8614cfa4cba3badcec9a450e9f3c3d4e83a
juanluca <juanlucaetena@gmail.com>
2022-05-16 19:40:16 +0200

    Conexion con BBDD añadida

--- app.js
+++ app.js
@@ -4,6 +4,31 @@ const hbs = require("hbs");
 const router = express.Router();
 const path = require("path");
 const bodyParser = require("body-parser");
+const MongoClient = require("mongodb").MongoClient;
+const url = "mongodb://localhost/test";
+const crypto = require("crypto");
+const util = require("util");
+const randomBytes = crypto.randomBytes;
+
+const Pokedex = require("pokeapi-js-wrapper");
+const P = new Pokedex.Pokedex();
+
+/*
+const interval = {
+  offset: 1,
+  limit: 1,
+};
+var pokemones = JSON.stringify(P.getPokemonsList(interval));
+var j;
+for (j; j < 2; j++) {
+  console.log(pokemones.count);
+}
+*/
+/*
+P.getPokemonSpeciesByName(25).then(function (response) {
+  console.log(response);
+});
+*/
 
 var inicioSesionIncorrecto = false;
 var sessionGuardada;
@@ -11,16 +36,18 @@ var sessionGuardada;
 //Sesiones: este es el codigo necesario para las sesiones
 const session = require("express-session");
 
-app.use(session({
+app.use(
+  session({
     secret: "clavesecretajkdsfn904309gtfi",
     resave: false,
-    saveUninitialized: false
-}));
+    saveUninitialized: false,
+  })
+);
 
 const res = require("express/lib/response");
 const req = require("express/lib/request");
 const cookieParser = require("cookie-parser");
-
+const { range } = require("express/lib/request");
 
 app.set("views", __dirname + "/src/views");
 app.set("view engine", "hbs");
@@ -29,207 +56,204 @@ hbs.registerPartials(__dirname + "/src/views/partials");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/", router);
 
+//  CONEXION CON MONGO
+
+MongoClient.connect(url, function (err, db) {
+  console.log("Conectado a MongoDB");
+  db.close();
+});
 
 //  -----------------------------------------------------------------------------------
 //  RENDERIZACION DE PAGINAS
 
 //  Pagina de login
 app.get("/", (req, res, next) => {
-    res.render("index", {layout: false});
+  res.render("index", { layout: false });
 });
 
 //  Pagina de registro
 app.get("/registro", (req, res, next) => {
-    res.render("registro", {layout: false});
+  res.render("registro", { layout: false });
 });
 
 //  Pagina de inicio
 app.get("/inicio", (req, res, next) => {
-
-    //POKEMONS DE PRUEBA
-    const pokemons = [
-        {
-            name: "Charizard",
-            photo: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/006.png",
-            tipo: "Fuego"
-        },
-        {
-            name: "Blastoise",
-            photo: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/009.png",
-            tipo: "Agua"
-        },
-        {
-            name: "Pikachu",
-            photo: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png",
-            tipo: "Electrico"
-        },
-        
-    ];
-    if(!req.session.username){
-        res.redirect("/");
-    } else {
-        let data = {
-            username: req.session.username,
-            pokemons
-        }
-        res.render("inicio", data);      
-    }
+  //POKEMONS DE PRUEBA
+  const pokemons = [
+    {
+      name: "Charizard",
+      photo: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/006.png",
+      tipo: "Fuego",
+    },
+    {
+      name: "Blastoise",
+      photo: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/009.png",
+      tipo: "Agua",
+    },
+    {
+      name: "Pikachu",
+      photo: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png",
+      tipo: "Electrico",
+    },
+  ];
+  if (!req.session.username) {
+    res.redirect("/");
+  } else {
+    let data = {
+      username: req.session.username,
+      pokemons,
+    };
+    res.render("inicio", data);
+  }
 });
 
 //  Pagina de mis pokemons
-app.get("/mispokemons", (req, res, next) => {  
-        //POKEMONS DE PRUEBA
-    const pokemons = [
-        {
-            name: "Charizard",
-            photo: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/006.png",
-            tipo: "Fuego"
-        },
-        {
-            name: "Blastoise",
-            photo: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/009.png",
-            tipo: "Agua"
-        },
-        {
-            name: "Pikachu",
-            photo: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png",
-            tipo: "Electrico"
-        },
-    ];
-    if(!req.session.username){
-        res.redirect("/");
-    } else {
-        res.render("mispokemons", {pokemons});
-    }
+app.get("/mispokemons", (req, res, next) => {
+  //POKEMONS DE PRUEBA
+  const pokemons = [
+    {
+      name: "Charizard",
+      photo: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/006.png",
+      tipo: "Fuego",
+    },
+    {
+      name: "Blastoise",
+      photo: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/009.png",
+      tipo: "Agua",
+    },
+    {
+      name: "Pikachu",
+      photo: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png",
+      tipo: "Electrico",
+    },
+  ];
+  if (!req.session.username) {
+    res.redirect("/");
+  } else {
+    res.render("mispokemons", { pokemons });
+  }
 });
 //  Pagina añadir pokemons
 app.get("/maspokemons", (req, res, next) => {
-
-    //POKEMONS DE PRUEBA
-    const pokemons = [
-        {
-            name: "Bulbasaur",
-            photo: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png",
-            tipo: "Planta"
-        },
-        {
-            name: "Ivysaur",
-            photo: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/002.png",
-            tipo: "Planta"
-        },
-        {
-            name: "Venusaur",
-            photo: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/003.png",
-            tipo: "Planta"
-        },
-        {
-            name: "Charmander",
-            photo: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png",
-            tipo: "Fuego"
-        },
-        {
-            name: "Charmeleon",
-            photo: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/005.png",
-            tipo: "Fuego"
-        },
-        {
-            name: "Charizard",
-            photo: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/006.png",
-            tipo: "Fuego"
-        },
-        {
-            name: "Squirtle",
-            photo: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png",
-            tipo: "Agua"
-        },
-        {
-            name: "Wartortle",
-            photo: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/008.png",
-            tipo: "Agua"
-        },
-        {
-            name: "Blastoise",
-            photo: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/009.png",
-            tipo: "Agua"
-        },
-        {
-            name: "Pikachu",
-            photo: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png",
-            tipo: "Electrico"
-        },
-
-    ];
-    if(!req.session.username){
-        res.redirect("/");
-    } else {
-        res.render("maspokemons", {pokemons});
-    }
+  //POKEMONS DE PRUEBA
+  const pokemons = [
+    {
+      name: "Bulbasaur",
+      photo: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png",
+      tipo: "Planta",
+    },
+    {
+      name: "Ivysaur",
+      photo: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/002.png",
+      tipo: "Planta",
+    },
+    {
+      name: "Venusaur",
+      photo: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/003.png",
+      tipo: "Planta",
+    },
+    {
+      name: "Charmander",
+      photo: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png",
+      tipo: "Fuego",
+    },
+    {
+      name: "Charmeleon",
+      photo: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/005.png",
+      tipo: "Fuego",
+    },
+    {
+      name: "Charizard",
+      photo: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/006.png",
+      tipo: "Fuego",
+    },
+    {
+      name: "Squirtle",
+      photo: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png",
+      tipo: "Agua",
+    },
+    {
+      name: "Wartortle",
+      photo: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/008.png",
+      tipo: "Agua",
+    },
+    {
+      name: "Blastoise",
+      photo: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/009.png",
+      tipo: "Agua",
+    },
+    {
+      name: "Pikachu",
+      photo: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png",
+      tipo: "Electrico",
+    },
+  ];
+  if (!req.session.username) {
+    res.redirect("/");
+  } else {
+    res.render("maspokemons", { pokemons });
+  }
 });
 
 //  Ver pokemon
 app.get("/verpokemon", (req, res, next) => {
-    if(!req.session.username){
-        res.redirect("/");
-    } else {
-        res.render("verpokemon");
-    }
+  if (!req.session.username) {
+    res.redirect("/");
+  } else {
+    res.render("verpokemon");
+  }
 });
 
 //  Pagina cuenta
 app.get("/cuenta", (req, res, next) => {
-    if(!req.session.username){
-        res.redirect("/");
-    } else {    
-        res.render("cuenta", req.session);
-    }
+  if (!req.session.username) {
+    res.redirect("/");
+  } else {
+    res.render("cuenta", req.session);
+  }
 });
 
 //  Pagina contacto
 app.get("/contacto", (req, res, next) => {
-    if(!req.session.username){
-        res.redirect("/");
-    } else {    
-        res.render("contacto");
-    }
+  if (!req.session.username) {
+    res.redirect("/");
+  } else {
+    res.render("contacto");
+  }
 });
 
 //  Not found get
 app.get("/*", (req, res, next) => {
-    if(!req.session.username){
-        res.redirect("/");
-    } else {    
-        let dataNotFound = {
-            notFoundImage:
-            "https://images6.alphacoders.com/109/1094097.png",
-        };
+  if (!req.session.username) {
+    res.redirect("/");
+  } else {
+    let dataNotFound = {
+      notFoundImage: "https://images6.alphacoders.com/109/1094097.png",
+    };
     res.render("404", dataNotFound);
-    }
+  }
 });
 //  -----------------------------------------------------------------------------------
 
-
 //  Codigo para hacer login
 app.post("/login", (req, res, next) => {
-    if(req.body.inputUsername == "rodri"){ //Consulta a BBDD
-        req.session.username = req.body.inputUsername;
-        req.session.save(function(err) {
-            res.redirect("/inicio");
-        })
-    }else{
-        inicioSesionIncorrecto = true;
-        res.render("index", {inicioSesionIncorrecto, layout:false});
-    }
-
+  if (req.body.inputUsername == "rodri") {
+    //Consulta a BBDD
+    req.session.username = req.body.inputUsername;
+    req.session.save(function (err) {
+      res.redirect("/inicio");
+    });
+  } else {
+    inicioSesionIncorrecto = true;
+    res.render("index", { inicioSesionIncorrecto, layout: false });
+  }
 });
 //  Codigo para registrarse
-app.post("/registro", (req, res, next) => {
-    
-
-});
+app.post("/registro", (req, res, next) => {});
 //  Codigo para cerrar sesion
 router.get("/logout", (req, res, next) => {
-    req.session.destroy();
-    res.redirect("/");      
+  req.session.destroy();
+  res.redirect("/");
 });
 
 app.use(cookieParser());
